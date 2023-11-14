588188e61d4588980e1f4063433c6442d50db8d4
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-11-14 11:02:26 -0600

    add gemfile

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,21 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all.page(params[:page]).per(15)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def search
+    @keywords = params[:keywords]
+    @category_id = params[:category_id]
+
+    @products = Product.all
+
+    @products = @products.where("name LIKE ?", "%#{@keywords}%") if @keywords.present?
+    return unless @category_id.present? && @category_id != "All Categories"
+
+    @products = @products.where(category_id: @category_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
   get 'products/index'
   get 'products/show'
   get 'products/search'
