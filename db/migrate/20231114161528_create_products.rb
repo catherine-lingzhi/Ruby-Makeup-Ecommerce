f29ccadb01e59b44a2698b9b3e331c77acb2351a
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-11-14 10:15:51 -0600

    Add product model

---
+++ db/migrate/20231114161528_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.string :image_link
+      t.text :description
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
