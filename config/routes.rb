46af4c3caf0e076ad75de95164b6f5056f3e89a2
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-11-19 13:45:27 -0600

    add admin login in nav and change show product go back link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
+
   root "products#index"
 
   resources :products, only: %i[index show] do
