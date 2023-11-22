bbdae421b4ae6af5d1640321660ddcf6e213c630
Jolene Kaminski <jolene.april14@gmail.com>
2023-11-22 14:42:56 -0600

    Added some columns to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_21_005351) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_204150) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -90,6 +90,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_21_005351) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
