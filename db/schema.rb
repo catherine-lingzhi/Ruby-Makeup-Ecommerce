3b9d46138fd2160525d061914e750221fe5f62c2
Jolene Kaminski <jolene.april14@gmail.com>
2023-11-23 11:47:57 -0600

    Removed address, first name and last name from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_23_174152) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_23_174714) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -129,9 +129,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_174152) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
