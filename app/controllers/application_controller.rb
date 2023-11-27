e8e21730e2956424af961333181ee862aa0e2a07
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-11-27 12:53:06 -0600

    add session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+
+  def initialize_session
+    session[:shopping_cart] ||= [] # empth array of products IDs
+  end
+
+  def cart
+    # lookup a product based upon a series of ids
+    Product.find(session[:shopping_cart])
+  end
 end
