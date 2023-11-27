35087fc6b10e1243fd0f0f464a16a8fbf576305e
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-11-27 14:33:35 -0600

    change cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :cart, only: %i[create destroy]
   root "products#index"
-
+  resources :carts, only: [:show]
   resources :products, only: %i[index show] do
     collection do
       get "search", to: "products#search"
