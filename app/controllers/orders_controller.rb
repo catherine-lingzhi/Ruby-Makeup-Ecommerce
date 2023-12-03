c559e2c07bba69cf962a02103b478c2145ad2489
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-12-03 12:59:04 -0600

    Implement cart session to the order and order detail table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,47 +1,34 @@
 # app/controllers/orders_controller.rb
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
-
   def create
-    @order = current_user.orders.new(subtotal: calculate_subtotal_price)
-
+    @order = Order.create(order_status: 0, subtotal: 0, user_id: current_user.id)
+    total_price = 0
     if @order.save
-      transfer_cart_details_to_order(@order)
-      clear_cart
-      redirect_to root_path, notice: "Order was successfully created."
+      Rails.logger.debug("Order saved successfully: #{@order.inspect}")
+
+      session[:shopping_cart].each do |item|
+        product = Product.find_by(id: item["id"])
+        quantity = item["quantity"]
+        subtotal = product.price * quantity
+        total_price += subtotal
+        @order.order_details.create(
+          quantity:,
+          price:      product.price,
+          product_id: product.id,
+          tax:        0.13
+        )
+      end
+      session[:shopping_cart] = nil
+      # Redirect or perform other actions as needed
     else
       # Handle errors
-      redirect_to root_path, alert: "Failed to create order."
     end
+    @order.update(subtotal: total_price)
   end
 
   private
 
-  def calculate_subtotal_price
-    # Implement the logic to calculate the total price
-    # You may have a method in your CartController to do this
-    # or you can iterate through the session[:shopping_cart] directly.
-  end
-
-  def transfer_cart_details_to_order(order)
-    session[:shopping_cart].each do |item|
-      product = Product.find_by(id: item["id"])
-
-      if product
-        order.order_details.create(
-          product_id: product.id,
-          quantity:   item["quantity"],
-          tax:        0.12
-        )
-      else
-        # Handle the case where the product is not found
-        # You might want to log an error or take appropriate action
-        Rails.logger.error("Product with ID #{item['id']} not found.")
-      end
-    end
-  end
-
-  def clear_cart
-    session[:shopping_cart] = []
+  def calculate_tax(price)
+    # Implement your tax calculation logic here
   end
 end
