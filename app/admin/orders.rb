c59c3bd872e534d096247e8bf5f8c77cccf60bbf
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-12-05 14:21:25 -0600

    add order and province in admin

---
+++ app/admin/orders.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Order do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :subtotal, :user_id, :order_status_id, :payment_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:subtotal, :user_id, :order_status_id, :payment_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/provinces.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Province do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :GST, :PST, :HST, :QST
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :GST, :PST, :HST, :QST]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
